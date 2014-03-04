63ac3e21e5881d0b02fff53d66ff9a6399d3bdf6
Benjamin Hilborn <ben.hilborn@gmail.com>
2014-03-04 07:58:07 -0700

    Initial Commit

---
+++ Course Selection Sort/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Course Selection Sort
+//
+//  Created by Benjamin Hilborn on 2014-03-04.
+//  Copyright (c) 2014 Benjamin Hilborn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
